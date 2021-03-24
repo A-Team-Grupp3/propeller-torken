1e0a55d7c499e43dac5b3965bc49b48b7b892b13
CarlMM <kaptenkeps@gmail.com>
2021-03-24 13:47:34 +0100

    Admincontroller +AdminPage layout + navbar styling css

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace propeller_torken.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Adminpage()
+        {
+            return View();
+        }
+
+    }
+}
