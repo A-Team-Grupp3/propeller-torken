b39ae925c5a53bb29cc34aca2c3eb66aac9f9d94
Carl Emil Lidbom <lidbom.calle@gmail.com>
2021-04-09 15:24:25 +0200

    Rebase commit

--- PropellerTorkenMain/wwwroot/js/ProductApiCall.js
+++ PropellerTorkenMain/wwwroot/js/ProductApiCall.js
@@ -1,7 +1,48 @@
-var ProductCall = /** @class */ (function () {
-    function ProductCall() {
-        this.uri = 'api/Product';
-    }
-    return ProductCall;
-}());
+var OrderUri = 'api/Order';
+var CustomerUri = 'api/Customer';
+var cookieData = sessionStorage.getItem('cart');
+function createOrder() {
+    var firstName = document.getElementById('firstName');
+    var lastName = document.getElementById('lastName');
+    var address = document.getElementById('street');
+    var city = document.getElementById('city');
+    var email = document.getElementById('email');
+    var phoneNumber = document.getElementById('phoneNumber');
+    var zipCode = document.getElementById('zipCode');
+    var custId = this.queryCustomer(firstName); // Kolla om kund
+    fetch(OrderUri, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(cookieData)
+    }).then(function (Response) { return Response.json(); })
+        .then(function (data) { return _displayItems(data); }).catch(function (error) { return console.error("Couldn't complete.", error); });
+}
+function queryCustomer(customer) {
+    return fetch(this.CustomerUri + "?name=" + customer).then(function (response) { return response.json(); }).then(function (data) { return data[0].customerId; });
+}
+function _displayItems(data) {
+    var elem = document.getElementById('orderIdDiv');
+    var flasher = document.createElement('h4');
+    flasher.textContent = data;
+    elem.appendChild(flasher);
+}
+function getCookie(name) {
+    var nameLenPlus = (name.length + 1);
+    return document.cookie
+        .split(';')
+        .map(function (c) { return c.trim(); })
+        .filter(function (cookie) {
+        return cookie.substring(0, nameLenPlus) === name + "=";
+    })
+        .map(function (cookie) {
+        return decodeURIComponent(cookie.substring(nameLenPlus));
+    })[0] || null;
+}
+function getIdFromRequest(data) {
+    var jsondata = JSON.parse(data);
+    return jsondata.customerId;
+}
 //# sourceMappingURL=ProductApiCall.js.map
--- PropellerTorkenMain/wwwroot/js/ProductApiCall.ts
+++ PropellerTorkenMain/wwwroot/js/ProductApiCall.ts
@@ -1,3 +1,50 @@
-﻿class ProductCall {
-    uri = 'api/Product';
+﻿const OrderUri = 'api/Order';
+const CustomerUri = 'api/Customer';
+
+function createOrder() {
+    const firstName = document.getElementById('firstName');
+    const lastName = document.getElementById('lastName');
+    const address = document.getElementById('street');
+    const city = document.getElementById('city');
+    const email = document.getElementById('email');
+    const phoneNumber = document.getElementById('phoneNumber');
+    const zipCode = document.getElementById('zipCode');
+    const custId = this.queryCustomer(firstName); // Kolla om kund
+    fetch(OrderUri, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body:
+    }).then(Response => Response.json())
+        .then(data => _displayItems(data)).catch(error => console.error("Couldn't complete.", error));
+}
+function queryCustomer(customer: string) {
+    return fetch(this.CustomerUri + "?name=" + customer).then(response => response.json()).then(data => data[0].customerId);
+}
+
+function _displayItems(data: string): any {
+    const elem = document.getElementById('orderIdDiv');
+    const flasher = document.createElement('h4');
+    flasher.textContent = data;
+    elem.appendChild(flasher);
+}
+
+function getCookie(name: string): string | null {
+    const nameLenPlus = (name.length + 1);
+    return document.cookie
+        .split(';')
+        .map(c => c.trim())
+        .filter(cookie => {
+            return cookie.substring(0, nameLenPlus) === `${name}=`;
+        })
+        .map(cookie => {
+            return decodeURIComponent(cookie.substring(nameLenPlus));
+        })[0] || null;
+}
+
+function getIdFromRequest(data: any): any {
+    var jsondata = JSON.parse(data);
+    return jsondata.customerId;
 }
